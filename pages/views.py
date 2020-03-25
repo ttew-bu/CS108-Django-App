da965c284da8594b26ac5fca1d95d9fd6989c82f
Tristan Tew <ttew@bu.edu>
2020-03-24 21:21:16 -0500

    Started pages application

---
+++ pages/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from django.views.generic import TemplateView
+
+# Create your views here.
+class HomePageView(TemplateView):
+    '''A specialized version of TemplateView to display our home page.'''
+
+    template_name = "pages/home.html"
+
+class AboutPageView(TemplateView):
+    '''A specialized version of TemplateView to display our about page.'''
+
+    template_name = "pages/about.html"
+
+class SchedulePageView(TemplateView):
+    '''A specialized version of TemplateView to display our schedule page.'''
+
+    template_name = "pages/schedule.html"
