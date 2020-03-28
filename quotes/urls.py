565d341371fc1f321ec07c695775e8cc95718d9a
Tristan Tew <ttew@bu.edu>
2020-03-28 15:38:00 -0500

    Added use of DetailView to display one random quote

--- quotes/urls.py
+++ quotes/urls.py
@@ -2,10 +2,11 @@
 #description: direct URL requests to view functions
 
 from django.urls import path
-from .views import HomePageView # our class definition
+from .views import HomePageView, QuotePageView, RandomQuotePageView # our class definition
 
 urlpatterns = [
     #map the URL (empty string) to the view
-    path('', HomePageView.as_view(), name='home') # generic class-based view
-    
+    path('', RandomQuotePageView.as_view(), name="random"), #pick random quote
+    path('all', HomePageView.as_view(), name='home'), # generic class-based view
+    path('quote/<int:pk>', QuotePageView.as_view(), name='quote'), #show one quote
 ]
--- quotes/views.py
+++ quotes/views.py
@@ -3,11 +3,35 @@ from django.shortcuts import render
 # Create your views here.
 
 from .models import Quote
-from django.views.generic import ListView
-
+from django.views.generic import ListView, DetailView
+import random
 class HomePageView(ListView):
     '''Create a subclass of ListView to display all quotes'''
 
     model = Quote #retrieve objects of type Quote from the db
     template_name = 'quotes/home.html'
     context_object_name = 'all_quotes_list' #how to find the template file
+
+class QuotePageView(DetailView):
+    '''Show the details for one quote'''
+    model = Quote
+    template_name = 'quotes/quote.html'
+    context_object_name = 'quote'
+
+class RandomQuotePageView(DetailView):
+    '''Show a random quote's details'''
+    model = Quote
+    template_name = 'quotes/quote.html'
+    context_object_name = 'quote'
+
+    # pick one at random
+    def get_object(self):
+        '''return a single instance of the Quote object, randomly selected'''
+
+        #get all quotes
+        all_quotes = Quote.objects.all()
+
+        #pick one at random
+        r = random.randint(0, (len(all_quotes)-1))
+        q = all_quotes[r]
+        return q #return this object
